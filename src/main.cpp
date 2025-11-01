5323c3342b4d24655fcd066e66433487f9094556
Tomasz Dobrowolski <chrumck@gmail.com>
2025-11-01 13:32:42 +0100

    format main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <HX711.h>   
+#include <HX711.h>
 
 #define LOAD_CELL_DOUT_PIN 0
 #define LOAD_CELL_SCK_PIN 1
 #define LOAD_CELL_TARE_REPS 10
 #define LOAD_CELL_SCALING 660000.0
 
-#define DAC_ADDR 0x60   
+#define DAC_ADDR 0x60
 const uint8_t DAC_CTRL_BYTE_WRITE = 0b01000000;
 
 #define V_VCC 3.3
@@ -26,24 +26,24 @@ const float DAC_CORRECTION_OFFSET = DAC_CORRECTION_CUTOFF * (1 - DAC_CORRECTION_
 
 HX711 lcReader;
 float lcValue;
-uint16_t dacValue; 
+uint16_t dacValue;
 
 void setup()
 {
   lcReader.begin(LOAD_CELL_DOUT_PIN, LOAD_CELL_SCK_PIN);
   lcReader.tare(LOAD_CELL_TARE_REPS);
   Wire.begin();
-} 
+}
 
 void loop()
 {
   lcValue = lcReader.get_value(1) / LOAD_CELL_SCALING;
-  if(lcValue < 0){ lcValue = 0;      }
-  if (lcValue > 1){ lcValue = 1; } 
+  if (lcValue < 0) { lcValue = 0; }
+  if (lcValue > 1) { lcValue = 1; }
 
-  if (lcValue > DAC_CORRECTION_CUTOFF){ lcValue = lcValue *DAC_CORRECTION_FACTOR + DAC_CORRECTION_OFFSET; }
+  if (lcValue > DAC_CORRECTION_CUTOFF) { lcValue = lcValue * DAC_CORRECTION_FACTOR + DAC_CORRECTION_OFFSET; }
 
-  dacValue = DAC_BRAKE_OFF - (DAC_RANGE *  lcValue);
+  dacValue = DAC_BRAKE_OFF - DAC_RANGE * lcValue;
   if (dacValue * DAC_STEP > V_BRAKE_OFF) { dacValue = V_BRAKE_OFF; }
 
   Wire.beginTransmission(DAC_ADDR);
