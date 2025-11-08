f64b68f9e195179250f79e0f649e978380d8d129
Tomasz Dobrowolski <chrumck@gmail.com>
2025-11-08 14:44:26 +0100

    first running code, time to calibrate

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 #include <Wire.h>
 #include <HX711.h>
 
-#define LOAD_CELL_DOUT_PIN 0
-#define LOAD_CELL_SCK_PIN 1
+#define LOAD_CELL_DOUT_PIN PD3
+#define LOAD_CELL_SCK_PIN PD2
 #define LOAD_CELL_TARE_REPS 10
 #define LOAD_CELL_SCALING 660000.0
 
 #define DAC_ADDR 0x60
 const uint8_t DAC_CTRL_BYTE_WRITE = 0b01000000;
 
-#define V_VCC 3.3
+#define V_VCC 3.26
 #define V_BRAKE_OFF 2.93
-#define V_BRAKE_FULL 1.48
+#define V_BRAKE_FULL 1.10
 
 #define DAC_RESOLUTION 4096
 #define DAC_STEP (V_VCC / DAC_RESOLUTION)
@@ -30,17 +30,30 @@ uint16_t dacValue;
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println("Logitech Wheel Brake Controller Starting...");
+
   lcReader.begin(LOAD_CELL_DOUT_PIN, LOAD_CELL_SCK_PIN);
+  Serial.println("LC reader initialized...");
+  
   lcReader.tare(LOAD_CELL_TARE_REPS);
+  Serial.println("LC zeroed...");
+
   Wire.begin();
 }
 
 void loop()
 {
-  lcValue = lcReader.get_value(1) / LOAD_CELL_SCALING;
+  lcValue = lcReader.get_value(2) / LOAD_CELL_SCALING;
+
   if (lcValue > DAC_CORRECTION_START) { lcValue = lcValue * DAC_CORRECTION_FACTOR + DAC_CORRECTION_OFFSET; }
   if (lcValue < 0) { lcValue = 0; }
   if (lcValue > 1) { lcValue = 1; }
+
+  Serial.print(">lcValue:");
+  Serial.print(millis());
+  Serial.print(":");
+  Serial.println(lcValue, 3);
 
   dacValue = DAC_BRAKE_OFF - DAC_RANGE * lcValue;
 
